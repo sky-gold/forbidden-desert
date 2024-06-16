f56c3449fe1a186de15fbbe2348065acd5f93364
Nikita Efimov <nikita-efimov04@mail.ru>
2024-06-17 00:00:17 +0300

    add connection pool

--- game_logic/src/helpers/env_vars.h
+++ game_logic/src/helpers/env_vars.h
@@ -5,6 +5,10 @@
 namespace EnvVars {
 
 extern std::string BOT_TOKEN;
+extern std::string DB_HOST;
+extern std::string DB_NAME;
+extern std::string DB_USER;
+extern std::string DB_PASSWORD;
 
 void init();
 
--- game_logic/src/main.cpp
+++ game_logic/src/main.cpp
@@ -1,16 +1,23 @@
+#include "handlers/get_create.h"
+#include "helpers/connection_pool.h"
 #include "helpers/env_vars.h"
 #include "middlewares/auth.h"
 
 #include <crow.h>
+#include <crow/app.h>
+#include <crow/http_request.h>
 #include <crow/logging.h>
 #include <crow/middleware_context.h>
+#include <string>
 
 int main() {
   EnvVars::init();
+  db_init();
 
   crow::App<AuthGuard> app;
 
-  CROW_ROUTE(app, "/")([]() { return "Hello, World!"; });
+  CROW_ROUTE(app, "/create")
+  ([&app](const crow::request &req) { return createHandler(app, req); });
 
   app.port(18080).multithreaded().run();
   return 0;
